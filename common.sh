2338c17922ad2ed0a62ef01382dc4b0f26a6a480
sai-chandana-ravuri <saichandanaravuri@gmail.com>
2026-04-23 19:54:43 +0530

    code changes

--- common.sh
+++ common.sh
@@ -13,7 +13,7 @@ START_TIME=$(date +%s)
 
 mkdir -p $LOGS_FOLDER
 
-echo "$(date "+%Y-%M-%D %H:%M:%S) | Script started executing at: $(date)" | tee -a $LOGS_FILE
+echo "$(date "+%Y-%m-%d %H:%M:%S") | Script started executing at: $(date)" | tee -a $LOGS_FILE
 check_root(){
     if [ $USER_ID -ne 0 ]; then
         echo -e "$R Please use admin access to install.. $N" | tee -a $LOGS_FILE
@@ -26,15 +26,15 @@ check_root(){
 
 VALIDATE(){
 if [ $1 -eq 0 ]; then
-   echo -e "$(date "+%Y-%M-%D %H:%M:%S) | $2...$G SUCCESS $N" | tee -a $LOGS_FILE
+   echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $2...$G SUCCESS $N" | tee -a $LOGS_FILE
 else
-   echo -e "$(date "+%Y-%M-%D %H:%M:%S) | $2...$R FAILURE $N" | tee -a $LOGS_FILE
+   echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $2...$R FAILURE $N" | tee -a $LOGS_FILE
    exit 1
 fi
 }
 
 print_total_time(){
     END_TIME=$(date +%s)
-    TOTAL_TIME=$(( $START_TIME-$END_TIME ))
-    echo -e "$(date "+%Y-%M-%D %H:%M:%S) | Script executed in: $G $TOTAL_TIME seconds $N" | tee -a $LOGS_FILE
+    TOTAL_TIME=$(( $END_TIME-$START_TIME ))
+    echo -e "$(date "+%Y-%m-%d %H:%M:%S") | Script executed in: $G $TOTAL_TIME seconds $N" | tee -a $LOGS_FILE
 }

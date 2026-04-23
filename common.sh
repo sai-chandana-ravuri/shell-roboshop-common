63aecbdffdb05562686701fa05959f343b7c2e0e
sai-chandana-ravuri <saichandanaravuri@gmail.com>
2026-04-23 19:50:25 +0530

    code changes

--- common.sh
+++ common.sh
@@ -10,6 +10,9 @@ Y='\e[33m'
 B='\e[34m'
 N='\e[0m'
 START_TIME=$(date +%s)
+
+mkdir -p $LOGS_FOLDER
+
 echo "$(date "+%Y-%M-%D %H:%M:%S) | Script started executing at: $(date)" | tee -a $LOGS_FILE
 check_root(){
     if [ $USER_ID -ne 0 ]; then
@@ -20,7 +23,6 @@ check_root(){
     fi
 }
 
-mkdir -p $LOGS_FOLDER
 
 VALIDATE(){
 if [ $1 -eq 0 ]; then
@@ -32,7 +34,7 @@ fi
 }
 
 print_total_time(){
-END_TIME=$(date +%s)
-TOTAL_TIME=$(( $START_TIME-$END_TIME ))
-echo -e "$(date "+%Y-%M-%D %H:%M:%S) | Script executed in: $G $TOTAL_TIME seconds $N" | tee -a $LOGS_FILE
+    END_TIME=$(date +%s)
+    TOTAL_TIME=$(( $START_TIME-$END_TIME ))
+    echo -e "$(date "+%Y-%M-%D %H:%M:%S) | Script executed in: $G $TOTAL_TIME seconds $N" | tee -a $LOGS_FILE
 }

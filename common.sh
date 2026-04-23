fe17508e013a41cbeb5b61f31e83495aaee91319
sai-chandana-ravuri <saichandanaravuri@gmail.com>
2026-04-23 20:17:20 +0530

    code changes

--- common.sh
+++ common.sh
@@ -3,6 +3,7 @@
 USER_ID=$(id -u)
 LOGS_FOLDER="/var/log/shell-roboshop"
 LOGS_FILE="/var/log/shell-roboshop/$0.log"
+SCRIPT_DIR=$PWD
 
 R='\e[31m'
 G='\e[32m'
@@ -10,6 +11,7 @@ Y='\e[33m'
 B='\e[34m'
 N='\e[0m'
 START_TIME=$(date +%s)
+MONGODB_HOST=mongodb.daws88c.online
 
 mkdir -p $LOGS_FOLDER
 
@@ -37,4 +39,59 @@ print_total_time(){
     END_TIME=$(date +%s)
     TOTAL_TIME=$(( $END_TIME-$START_TIME ))
     echo -e "$(date "+%Y-%m-%d %H:%M:%S") | Script executed in: $G $TOTAL_TIME seconds $N" | tee -a $LOGS_FILE
+}
+
+nodejs_setup(){
+    dnf module disable nodejs -y &>>$LOGS_FILE
+    VALIDATE $? "Disabling NodeJS Default version"
+
+    dnf module enable nodejs:20 -y &>>$LOGS_FILE
+    VALIDATE $? "Enabling NodeJS 20"
+
+    dnf install nodejs -y &>>$LOGS_FILE
+    VALIDATE $? "Install NodeJS"
+
+    npm install  &>>$LOGS_FILE
+    VALIDATE $? "Installing dependencies"
+}
+
+app_setup(){
+    #Creating system user
+    id roboshop &>>$LOGS_FILE
+    if [ $? -ne 0 ]; then
+        useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
+        VALIDATE $? "Creating system user"
+    else
+        echo -e "Roboshop user already exist ... $Y SKIPPING $N"
+    fi
+    #Downnloading app and unzipping it
+    mkdir -p /app 
+    VALIDATE $? "Creating app directory"
+
+    curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip  &>>$LOGS_FILE
+    VALIDATE $? "Downloading $app_name code"
+
+    cd /app
+    VALIDATE $? "Moving to app directory"
+
+    rm -rf /app/*
+    VALIDATE $? "Removing existing code"
+
+    unzip /tmp/$app_name.zip &>>$LOGS_FILE
+    VALIDATE $? "Uzip $app_name code"
+}
+
+systemd_setup(){
+    cp $SCRIPT_DIR/$app_name.service /etc/systemd/system/$app_name.service
+    VALIDATE $? "Created systemctl service"
+
+    systemctl daemon-reload
+    systemctl enable $app_name  &>>$LOGS_FILE   
+    systemctl start $app_name
+    VALIDATE $? "Starting and enabling $app_name"
+}
+
+app_restart(){
+    systemctl restart $app_name
+    VALIDATE $? "Restarting $app_name"
 }

61577953e071ed94ef799e10a134b9467970293d
sai-chandana-ravuri <saichandanaravuri@gmail.com>
2026-05-08 15:46:41 +0530

    code changes

--- common.sh
+++ common.sh
@@ -68,6 +68,15 @@ java_setup(){
     VALIDATE $? "Moving and renaming $app_name"
 }
 
+python_setup(){
+    dnf install python3 gcc python3-devel -y &>>$LOGS_FILE
+    VALIDATE $? "Installing python"
+
+    cd /app 
+    pip3 install -r requirements.txt
+    VALIDATE $? "Installing dependencies"
+}
+
 app_setup(){
     #Creating system user
     id roboshop &>>$LOGS_FILE

981cec50f0fefd965caf413d0b276c71b9e65a2d
sai-chandana-ravuri <saichandanaravuri@gmail.com>
2026-05-08 16:20:43 +0530

    code changes

--- shipping.sh
+++ shipping.sh
@@ -12,7 +12,7 @@ dnf install mysql -y &>>$LOGS_FILE
 VALIDATE $? "Installing mysql"
 
 mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities'
-if [ $? -nq 0]; then
+if [ $? -nq 0 ]; then
    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOGS_FILE
    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOGS_FILE
    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOGS_FILE
